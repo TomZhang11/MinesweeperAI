54b24f7f0c32790428e9151d57a1c18d6ec3e454
Tom Zhang <tomzhang.canada888@gmail.com>
2025-07-14 16:57:07 -0400

    minor changes

---
+++ src/util/settings.py
@@ -0,0 +1,20 @@
+import pyautogui
+
+# centralized settings
+
+class Settings:
+    SPEED = "medium" # fast medium slow
+
+    # the number of unrecognized cell colors before throwing an exception
+    UNRECOGNIZE_COUNT = 70
+
+    SLEEP_TIME = 2
+
+    if SPEED == "fast":
+        SLEEP_TIME = 0.7
+        UNRECOGNIZE_COUNT = 30
+    elif SPEED == "medium":
+        SLEEP_TIME = 1
+        UNRECOGNIZE_COUNT = 50
+
+    pyautogui.PAUSE = 0
